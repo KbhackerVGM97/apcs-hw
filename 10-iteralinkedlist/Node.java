be94cdcc46389353ee1ffa949d64e3364b46581f
Kemraj Sobhai <kbsobhai@gmail.com>
2014-03-26 18:26:33 -0400

    10

---
+++ 10-iteralinkedlist/Node.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Node{
+    private String data;
+    private Node next;
+    
+    public Node(String info){
+	data = info;
+    }
+
+    public void setData(String info){
+	data = info;
+    }
+
+    public String getData(){
+	return data;
+    }
+
+    public void setNext(Node follower){
+	next = follower;
+    }
+
+    public Node getNext(){
+	return next;
+    }
+
+    public String toString(){
+	return "Data: "+data;//+" Next: "+next;
+    }
+}
