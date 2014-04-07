d51a5eb2d8d9f08b262d0c46cfc9875cc5bdf039
Kemraj Sobhai <kbsobhai@gmail.com>
2014-04-07 08:39:57 -0400

    class work added

---
+++ 13-RPN/RPNCalc.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class RPNCalc{
+    public static void main (String[] args){
+	MyStackDouble data = new MyStackDouble();
+	scanner input = new scanner(System.in);
+	String lastIn;
+	while (true){
+	    //do the stuff and return the ans
+	    lastIn = lastIn.nextLine();
+	}
+    }
+}
