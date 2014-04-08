ebb92248e7e048ebf152ad66f627ec3863295e83
Kemraj Sobhai <kbsobhai@gmail.com>
2014-04-08 08:21:23 -0400

    Aworks now

--- 13-RPN/RPNCalc.java
+++ 13-RPN/RPNCalc.java
@@ -3,32 +3,37 @@ import java.util.*;
 // I keep getting  cannot find lastln error whenever i compile, and i cant find the issue
 public class RPNCalc{
     public static void main (String[] args){
-		MyStackDouble data = new MyStackDouble();
-		Scanner sc = new Scanner(System.in);
-		while (true){
-			//do the stuff and return the ans
-			String lastIn = sc.nextLine();
-			if (lastln == "+"){
-				data.push(data.pop()+data.pop());
-			}
-			if (lastln == "-"){
-				data.push(data.pop()-data.pop());
-			}
-			if (lastln == "/"){
-				data.push(data.pop()/data.pop());
-			}
-			if (lastln == "*"){
-				data.push(data.pop()*data.pop());
-			}
-			else{
-				try{
-					data.push(Integer.parseDouble(lastln));
-				}
-				catch(Exception e){
-					System.out.println("Whoops");
-				}
-			}
+	MyStackDouble data = new MyStackDouble();
+	Scanner sc = new Scanner(System.in);
+	while (true){
+	    //do the stuff and return the ans
+	    String lastln = sc.nextLine();
+	    System.out.println ("."+lastln+".");
+	    if (lastln.equals("+")){
+		data.push(data.pop()+data.pop());
+		System.out.println(data.peek());
+	    }
+	    else if (lastln.equals("-")){
+		data.push(data.pop()-data.pop());
+		System.out.println(data.peek());
+	    }
+	    else if (lastln.equals("/")){
+		data.push(data.pop()/data.pop());
+		System.out.println(data.peek());
+	    }
+	    else if (lastln.equals("*")){
+		data.push(data.pop()*data.pop());
+		System.out.println(data.peek());
+	    }
+	    else{
+		//try{
+		    data.push(Double.parseDouble(lastln));
+		    //}
+		    //catch(Exception e){
+		    //System.out.println("Whoops");
+		    //}
+	    }
 			
-		}
+	}
     }
 }
