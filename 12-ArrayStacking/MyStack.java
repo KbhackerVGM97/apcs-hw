794d7737a7ef1765e2b8f889293923817f9c0cd6
Kemraj Sobhai <kbsobhai@gmail.com>
2014-04-23 08:36:57 -0400

    node added trees

--- 12-ArrayStacking/MyStack.java
+++ 12-ArrayStacking/MyStack.java
@@ -3,47 +3,47 @@ import java.util.*;
 
 public class MyStack{
     private int index;
-	private String[] items;
+    private String[] items;
 
     public MyStack(){
-		items = new String[10];
-		index=0;
+	items = new String[10];
+	index=0;
     }
 
-	private void grow(){
-		String[] tmp = new String[items.length + (items.length / 2)];
-		for (int i = 0; i < items.length; i++)
-			tmp[i]=items[i];
-		items=tmp;
-	}
+    private void grow(){
+	String[] tmp = new String[items.length + (items.length / 2)];
+	for (int i = 0; i < items.length; i++)
+	    tmp[i]=items[i];
+	items=tmp;
+    }
 	
     public void push(String s){
-		if (index==items.length)
-			grow();
-		items[index]=s;
-		index++;
+	if (index==items.length)
+	    grow();
+	items[index]=s;
+	index++;
     }    
 
     public String pop(){
-		String s = items[index];
-		index--;
-		return s;
+	String s = items[index];
+	index--;
+	return s;
     }
 
     public String peek(){
-		return items[index];
+	return items[index];
     }
     
-	public boolean isEmpty(){
-		return (index>0);
+    public boolean isEmpty(){
+	return (index>0);
     }
     
-	public String toString(){
-		String ans = "";
-		for (int i = 0;i<index;i++){
-			ans+=items[i];
-			ans+=", ";
-		}
-		return ans;
+    public String toString(){
+	String ans = "";
+	for (int i = 0;i<index;i++){
+	    ans+=items[i];
+	    ans+=", ";
+	}
+	return ans;
     }
 }
---
+++ 15-Tree/Node.java
@@ -0,0 +1,28 @@
+public class Node{
+    private int data;
+    private Node left, right;
+
+    public void  setRight(Node next){
+	right=next;
+    }
+
+    public void setLeft(Node next){
+	left = next;
+    }
+
+    public void setData(int stuff){
+	data=stuff;
+    }
+
+    public int getData(){
+	return data;
+    }
+
+    public Node getLeft(){
+	return left;
+    }
+
+    public Node getRight(){
+	return right;
+    }
+}
