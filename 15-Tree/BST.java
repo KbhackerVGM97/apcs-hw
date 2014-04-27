515dd948cfe76fa94becea8dfa95d8d42344893a
Kemraj Sobhai <Kbsobhai@gmail.com>
2014-04-27 15:17:20 -0400

    finished hw

---
+++ 15-Tree/BST.java
@@ -0,0 +1,14 @@
+public class BST{
+	private Node root;
+	
+	public BST(){
+		root=null;
+	}
+	
+	public void setRoot(Node next){
+		root = next;
+	}
+	public Node getRoot(){
+		return root;
+	}
+}
--- 15-Tree/Node.java
+++ 15-Tree/Node.java
@@ -2,27 +2,32 @@ public class Node{
     private int data;
     private Node left, right;
 
-    public void  setRight(Node next){
-	right=next;
+	public Node(int stuff){
+		data=stuff;
+		left=null;
+		right=null;
+	}
+    public void setRight(Node next){
+		right=next;
     }
 
     public void setLeft(Node next){
-	left = next;
+		left = next;
     }
 
     public void setData(int stuff){
-	data=stuff;
+		data=stuff;
     }
 
     public int getData(){
-	return data;
+		return data;
     }
 
     public Node getLeft(){
-	return left;
+		return left;
     }
 
     public Node getRight(){
-	return right;
+		return right;
     }
 }
