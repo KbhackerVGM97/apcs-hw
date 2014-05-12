e9ca56bcaa68ff79ee8b549c32673d9a988b8784
Kemraj Sobhai <Kbsobhai@gmail.com>
2014-05-12 02:27:44 -0400

    array index exception

--- 18-HeapSort/HeapSort.java
+++ 18-HeapSort/HeapSort.java
@@ -1,3 +1,15 @@
+import java.util.*;
+
 public class HeapSort{
-    
+    public static void main (String[] args){
+		Heap buddy = new Heap();
+		int[] datea = new int[20];
+		int i;
+		for (i = 0; i<20;i++)
+			datea[i] = (int) (Math.random() * i);
+		System.out.println(Arrays.toString(datea));
+		for (i=0;i<datea.length;i++)
+			buddy.add(datea[i]);
+		System.out.println(buddy);
+	}
 }
